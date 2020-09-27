04b18ae5e6f7d815ab89fd7c0433df7d939aa669
Simen Bekkhus <simenb@folio.no>
2020-09-27 13:42:34 +0200

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const { readFileSync } = require('fs');
+const { Script, compileFunction } = require('vm');
+
+const fileSource = readFileSync('./.yarn/releases/yarn-berry.cjs', 'utf8')
+  .split('\n')
+  // get rid of shebang
+  .slice(1)
+  .join('\n');
+
+const oneMillisecondAsNano = 1_000_000n;
+
+let start = process.hrtime.bigint();
+
+for (let i = 0; i < 100; i++) {
+  new Script(fileSource);
+}
+
+console.log(
+  'Running with Script took',
+  Number((process.hrtime.bigint() - start) / oneMillisecondAsNano),
+  'ms',
+);
+
+start = process.hrtime.bigint();
+
+for (let i = 0; i < 100; i++) {
+  compileFunction(fileSource);
+}
+
+console.log(
+  'Running with compileFunction took',
+  Number((process.hrtime.bigint() - start) / oneMillisecondAsNano),
+  'ms',
+);
